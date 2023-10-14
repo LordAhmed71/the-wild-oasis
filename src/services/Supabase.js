1d2868f30ebdfd8e22bece510b4d7c362ead3bcb
LordAhmed71 <ahmed55mohamed88@gmail.com>
2023-10-14 14:41:33 +0300

    get start

---
+++ src/services/Supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://tfkkdjoloqlcaitmzbhi.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRma2tkam9sb3FsY2FpdG16YmhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcwNDQ2MDAsImV4cCI6MjAxMjYyMDYwMH0.MO_CjRag6YT4opUQ2ifq1S0JNf3ospcYBuHS3scpwTw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
