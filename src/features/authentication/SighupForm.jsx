e09038cda65b0bb78338ee7331127a94ad7a66c5
LordAhmed71 <ahmed55mohamed88@gmail.com>
2023-10-30 23:55:14 +0200

    updating user data

--- src/features/authentication/SighupForm.jsx
+++ src/features/authentication/SighupForm.jsx
@@ -73,7 +73,12 @@ function SighupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset" disabled={isLoading}>
+        <Button
+          variation="secondary"
+          onClick={reset}
+          type="reset"
+          disabled={isLoading}
+        >
           Cancel
         </Button>
         <Button disabled={isLoading}>Create new user</Button>
